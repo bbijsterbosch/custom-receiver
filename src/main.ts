041514eaa131a3d2b0cceac08cabee48497e3493
Bas Bijsterbosch <basbijsterbosch97@gmail.com>
2026-03-19 22:50:36 +0100

    added wait function for initial fonts and image to load

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,10 @@ try {
 } catch (err) {
   console.error("[Main] Receiver initialization failed:", err);
 }
+// Wait for fonts and images to load before displaying the receiver
+window.addEventListener('load', () => {
+  // Wait for fonts and images to load
+  document.fonts.ready.then(() => {
+      document.body.classList.add('ready');
+  });
+});
