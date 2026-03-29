8a23e08579afad454253f1555caa966ed750c7b2
Bas Bijsterbosch <basbijsterbosch97@gmail.com>
2026-03-29 15:37:05 +0200

    test native player for the fps issue

--- src/receiver.ts
+++ src/receiver.ts
@@ -323,7 +323,6 @@ export function initializeReceiver(): void {
 
   const options = new cast.framework.CastReceiverOptions();
   options.disableIdleTimeout = false;
-  options.useShakaForHls = true;
 
   context.start(options);
   console.log("[Receiver] Streamyfin Cast Receiver started");
